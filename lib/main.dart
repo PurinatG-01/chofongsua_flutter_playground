08885bd3abcd95e451c4b78b1464d257fb8bcefb
Purinat Sanbundit <37452245+PurinatG-01@users.noreply.github.com>
2024-07-14 12:51:08 +0700

    chore: update code format

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
-  ThemeMode themeMode = ThemeMode.light;
+  final ThemeMode themeMode = ThemeMode.light;
   final seedColor = const Color.fromARGB(255, 0, 120, 92);
 
-   MyApp({super.key});
+  const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
@@ -33,16 +32,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
